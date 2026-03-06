1e161de55fdd602929943bd0aa0781f70baf04a1
Bennett Bullock <bennettbullock@Bennetts-MacBook-Pro.local>
2026-03-06 01:53:19 -0500

    Commented

--- schema.py
+++ schema.py
@@ -6,19 +6,26 @@ from datetime import date
 from typing import Optional,Literal,Annotated
 
 class Name(BaseModel):
-
+    '''
+    Object to represent a person by their first and last names.
+    '''
     firstName: str
     lastName: str
 
 
 class Deposit(BaseModel):
-
+    '''
+    Object to represent a deposit, indicating a floating-point value denominated in US dollars,
+    and a form through which the deposit is made - check, money order, transfer.
+    '''
     depositAmount: float
     form: Literal['check','money order','transfer']
 
 
 class Property(BaseModel):
-
+    '''
+    Object to represent the geographic information about the property in question.
+    '''
     streetNumber: int
     street: str
     city: str
@@ -28,7 +35,9 @@ class Property(BaseModel):
 
 
 class Contract(BaseModel):
-
+    '''
+    Object to represent the contract which is described in the JPEG file.
+    '''
     dt:  Optional[date]
     buyer: Optional[Name]
     seller: Optional[Name]
